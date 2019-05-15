9cfcc37353522fa4d58a196067889bcc83f9ade3
Patrick Kuria <kuria144@gmail.com>
2019-05-15 17:11:25 +0300

    Genesis

---
+++ game.py
@@ -0,0 +1,6 @@
+"""THIS WILL CONTAIN MULTIPLE WORD GAMES"""
+# First game is that of questions to see how much your partner knows you.
+# list of questions
+# The asker inputs his/her answers of the questions provided.
+# Then in random order, the partner is asked the questions, one by one they answer.
+# The answers of the asked are marked against the answers of the asker and the meaning of their result is revealed.
