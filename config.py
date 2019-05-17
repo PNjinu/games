9a4852a1ca2b399d7bd5d25a3f9f6e7a3f875eb0
Patrick Kuria <kuria144@gmail.com>
2019-05-17 09:32:10 +0300

    Hidden the sensitive information

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os
+
+if not os.getenv("GAMESDB_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "postgres://rhorbjqbilyivg:d21a06382bbf64da498543deb0692e08cd4f68e6b16843582049fca5e59ac9ce@ec2-23-23-228-132.compute-1.amazonaws.com:5432/db62nln4h7f707"
+SQLALCHEMY_DATABASE_URI = os.getenv("GAMESDB_URL")
 SECRET_KEY = "(wqkkkss1212@#!@!{si)Wqw+"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- import.py
+++ import.py
@@ -5,7 +5,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("postgres://rhorbjqbilyivg:d21a06382bbf64da498543deb0692e08cd4f68e6b16843582049fca5e59ac9ce@ec2-23-23-228-132.compute-1.amazonaws.com:5432/db62nln4h7f707")
+engine = create_engine(os.getenv("GAMESDB_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
